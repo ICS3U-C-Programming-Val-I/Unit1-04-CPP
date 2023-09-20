141085f5cdeeff7df5c74e39d64a6202a8d5f5b7
Val Ijaola <valian.ijaola@stu.ocsb.ca>
2023-09-20 04:07:39 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 Val I All rights reserved
+// .
+// Created By: Val Ijaola
+// Date: September 19, 2023
+// This program calculates the area and perimeter
+// of a rectangle with dimensions 8cm X 9cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "length 8cm and width 9cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 8 * 9 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (8 + 9) << " cm\n";
+}
---
+++ basic_math.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 Val I All rights reserved
+// .
+// Created By: Val Ijaola
+// Date: September 19, 2023
+// This program does basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+  std::cout << 5+8 << std::endl;
+  std::cout << 8-7 << std::endl;
+  std::cout << 8/2.5 << std::endl;
+  std::cout << 8*2 << std::endl;
+  std::cout << pow(4, 3) << std::endl;
+  std::cout << float(22)/float(7) << std::endl;
+}
